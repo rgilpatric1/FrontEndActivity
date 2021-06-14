4c9504c13085a831542ce8f32a5a5230c73114ab
Rebecca Gilpatric <rebeccagilpatric@gmail.com>
2021-06-13 22:01:59 -0500

    Project Creation

---
+++ Front End Activity/Front End Activity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Front_End_Activity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
