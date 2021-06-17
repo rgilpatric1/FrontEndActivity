0cc3c14c2c83fb18270e8f22fa2551cae1b420ff
Benjamin Kim <22kimb@elmbrookstudenst.org>
2021-06-17 12:58:57 -0500

    Ben's work 5

---
+++ Front End Activity/Front End Activity/HTML/Kim/BenSite.js
@@ -0,0 +1,18 @@
+﻿function showHide() {
+    var x = document.getElementById("vision");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+function showHide() {
+    var x = document.getElementById("history");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
